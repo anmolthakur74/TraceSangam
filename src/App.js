2c9f03f5b53168747889d1b2eced2b7b392d6954
Kairavipatra <166865579+Kairavipatra@users.noreply.github.com>
2025-06-26 21:26:02 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 // src/App.js
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Sidebar from "./components/Sidebar";
-import Topbar from "./components/Topbar"; // 
-import DashboardWidgets from "./components/DashboardWidgets";
-import Shipments from "./components/Shipments";
-import CompliancePanel from "./components/CompliancePanel";
-
+import Topbar from "./components/Topbar";
+import Dashboard from "./components/Dashboard"; 
+import ShipmentsPage from './pages/ShipmentsPage'; 
 export default function App() {
   return (
-    <div className="flex">
-      <Sidebar />
-      <div className="ml-64 flex-1 bg-gray-50 min-h-screen">
-        <Topbar /> 
-        <div className="p-6">
-          <DashboardWidgets />
-          <div className="grid grid-cols-3 gap-4">
-            <div className="col-span-2">
-              <Shipments />
-            </div>
-            <CompliancePanel />
-          </div>
+    <Router>
+      <div className="flex">
+        <Sidebar />
+        <div className="ml-64 flex-1 bg-gray-50 min-h-screen">
+          <Topbar />
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/shipments" element={<ShipmentsPage />} />
+          </Routes>
         </div>
       </div>
-    </div>
+    </Router>
   );
 }

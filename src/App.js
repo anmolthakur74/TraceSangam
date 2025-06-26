c36a10dc94c093012d579c63c8cbc74f98720238
Kairavipatra <166865579+Kairavipatra@users.noreply.github.com>
2025-06-26 21:11:15 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // src/App.js
 import Sidebar from "./components/Sidebar";
+import Topbar from "./components/Topbar"; // 
 import DashboardWidgets from "./components/DashboardWidgets";
 import Shipments from "./components/Shipments";
 import CompliancePanel from "./components/CompliancePanel";
@@ -8,13 +9,16 @@ export default function App() {
   return (
     <div className="flex">
       <Sidebar />
-      <div className="ml-64 p-6 bg-gray-50 min-h-screen w-full">
-        <DashboardWidgets />
-        <div className="grid grid-cols-3 gap-4">
-          <div className="col-span-2">
-            <Shipments />
+      <div className="ml-64 flex-1 bg-gray-50 min-h-screen">
+        <Topbar /> 
+        <div className="p-6">
+          <DashboardWidgets />
+          <div className="grid grid-cols-3 gap-4">
+            <div className="col-span-2">
+              <Shipments />
+            </div>
+            <CompliancePanel />
           </div>
-          <CompliancePanel />
         </div>
       </div>
     </div>

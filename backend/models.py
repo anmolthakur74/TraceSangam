9a29474f8c0f710a6909756723dd137c628dc7ad
Kairavipatra <166865579+Kairavipatra@users.noreply.github.com>
2025-06-28 17:06:39 +0530

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -6,4 +6,3 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
-
